906c252de5c7acb88c97e61f630f195ea996444c
sybwksl <pandu@sybwksl65.(none)>
2012-06-13 17:57:01 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PanduOnline::Application.config.secret_token = '64092804d76a9a0f837ff510e17e44440b7832895bcd23a88270bd19a83b6f371959cf6ec63e88eb1e98bfacda2d873d0e5925385c1dfd3cd94fe936726b43be'
