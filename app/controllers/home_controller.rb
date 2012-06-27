6c7879d9d4aa7c836bbbc4c0b191232272f44911
Radhika <radhika.p@sybrant.com>
2012-06-27 15:22:41 +0530

    added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+	
+	def home
+	end
+	
+	def about_us
+	end
+	
+	def contact_us
+	end
+	
+	def faq
+	end
+	
+	def gallery
+	end
+	
+	def volunteers
+	end
+	
+	def events
+	end
+	
+end
